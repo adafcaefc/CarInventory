3be52d74a4cb9e4a028f11b029868109e82d1e9a
adafcaefc <kjuanvincent@gmail.com>
2022-06-23 17:51:32 +0700

    Added component registration setup for DataRecordPool

--- src/Model/DataPool/DataRecordPool.java
+++ src/Model/DataPool/DataRecordPool.java
@@ -9,6 +9,57 @@ public class DataRecordPool
     private final ArrayList<DataRecord> componentObjects = new ArrayList<>();;
     private final DataRecordPool nextPool;
 
+    public DataRecord getComponentAt(int index)
+    {
+        return componentObjects.get(index);
+    }
+
+    public int getIndexForComponent(DataRecord component)
+    {
+        return componentObjects.indexOf(component);
+    }
+
+    public int countRegisteredComponents()
+    {
+        return componentObjects.size();
+    }
+
+    public boolean componentIsRegisteredAtPool(DataRecord object) { return componentObjects.contains(object); }
+    protected void cleanupOrphanedChildren()
+    {
+        if (nextPool == null) { return; }
+        for (int i = 0; i < nextPool.countRegisteredComponents(); )
+        {
+            DataRecord childComponent = nextPool.getComponentAt(i);
+            if (!componentIsRegisteredAtPool(childComponent.getParent()))
+            {
+                nextPool.unregisterComponent(childComponent);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        nextPool.cleanupOrphanedChildren();
+    }
+    public void registerComponent(DataRecord object)
+    {
+        if (!componentObjects.contains(object))
+        {
+            componentObjects.add(object);
+        }
+    }
+
+    public void unregisterComponent(DataRecord object)
+    {
+        if (componentObjects.contains(object))
+        {
+            object.removeFromParent();
+            object.removeAllChildrenRecursively();
+            componentObjects.remove(object);
+        }
+        cleanupOrphanedChildren();
+    }
     public DataRecordPool(DataRecordPool nextPool)
     {
         this.nextPool = nextPool;
