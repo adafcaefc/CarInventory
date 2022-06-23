3faa71aa6fb96e4cb0a07ab66bb0e591c4245546
David <95734746+david-manalu@users.noreply.github.com>
2022-06-23 20:10:22 +0700

    Added User DataRecord (#3)
    * Added User DataRecord
    
    * Added Getters Setters on User DataRecord
    
    * Added UserLevel enum
    
    * Added value for UserLevel enum
    
    * Added indexed access to UserLevel enum
    
    * Edited User DataRecord and added UserPool DataPool
    
    Co-authored-by: adafcaefc <kjuanvincent@gmail.com>

---
+++ src/Model/DataModel/User.java
@@ -0,0 +1,49 @@
+package Model.DataModel;
+
+public class User extends DataRecord
+{
+    private String userName;
+    private String password;
+    private String salt;
+    private UserLevel userLevel;
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getSalt()
+    {
+        return salt;
+    }
+
+    public void setSalt(String salt)
+    {
+        this.salt = salt;
+    }
+
+    public UserLevel getUserLevel()
+    {
+        return userLevel;
+    }
+
+    public void setUserLevel(UserLevel userLevel)
+    {
+        this.userLevel = userLevel;
+    }
+}
---
+++ src/Model/DataModel/UserLevel.java
@@ -0,0 +1,24 @@
+package Model.DataModel;
+
+import java.util.HashMap;
+
+public enum UserLevel 
+{
+    ADMIN(0),
+    SALES_MANAGER(1),
+    PRODUCT_MANAGER(2);
+
+    private final int value;
+    public static final HashMap<Integer, UserLevel> map = new HashMap<>();
+
+    UserLevel(int value) { this.value = value; }
+
+    static
+    {
+        for (UserLevel userLevel : UserLevel.values()) { map.put(userLevel.value, userLevel); }
+    }
+
+    public static UserLevel valueOf(int v) { return map.get(v); }
+
+    public int getValue() { return value; }
+}
