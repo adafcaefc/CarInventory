48ea420e16f42f0e3a78e3f02aebca2ae48ea3a2
Thomas Teguh <thmstgh@gmail.com>
2022-06-23 21:15:35 +0700

    Added getters and setters for Vehicle DataRecord

--- src/Model/DataModel/Vehicle.java
+++ src/Model/DataModel/Vehicle.java
@@ -1,7 +1,48 @@
-public class Vehicle 
+package Model.DataModel;
+public class Vehicle extends DataRecord
 {
     private String VIN;
     private String licensePlate;
     private String color;
     private Double mileage = 0.0;
+
+    public String getVIN()
+    {
+        return this.VIN;
+    }
+
+    public void setVIN(String numVIN)
+    {
+        this.VIN = numVIN;
+    }
+
+    public String getLicensePlate()
+    {
+        return this.licensePlate;
+    }
+
+    public void setLicensePlate(String newLicensePlate)
+    {
+        this.licensePlate = newLicensePlate;
+    }
+
+    public String getColor()
+    {
+        return this.color;
+    }
+
+    public void setColor(String newColor)
+    {
+        this.color = newColor;
+    }
+
+    public Double getMileage()
+    {
+        return this.mileage;
+    }
+
+    public void setMileage(Double newMillage)
+    {
+        this.mileage = newMillage;
+    }
 }
