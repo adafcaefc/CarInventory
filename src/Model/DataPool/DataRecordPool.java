4ad316447b2f7b482987a946c32993dae43a1072
adafcaefc <kjuanvincent@gmail.com>
2022-06-23 17:50:30 +0700

    Added constructor for DataRecordPool

--- src/Model/DataPool/DataRecordPool.java
+++ src/Model/DataPool/DataRecordPool.java
@@ -1,5 +1,16 @@
 package Model.DataPool;
 
+import Model.DataModel.DataRecord;
+
+import java.util.ArrayList;
+
 public class DataRecordPool
 {
+    private final ArrayList<DataRecord> componentObjects = new ArrayList<>();;
+    private final DataRecordPool nextPool;
+
+    public DataRecordPool(DataRecordPool nextPool)
+    {
+        this.nextPool = nextPool;
+    }
 }
