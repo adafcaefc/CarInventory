73497db728765f462408d3b2a4b9fcbbf8e83d7d
David <95734746+david-manalu@users.noreply.github.com>
2022-06-23 19:25:26 +0700

    Added Brand DataRecord

---
+++ src/Model/DataModel/Brand.java
@@ -0,0 +1,13 @@
+package Model.DataModel;
+
+public class Brand extends DataRecord {
+    private String brandName;
+
+    public String getBrandName() {
+        return this.brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+}
