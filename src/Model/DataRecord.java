4af817dfefd2a489a399e76c7fec3dc1134406f9
adafcaefc <kjuanvincent@gmail.com>
2022-06-23 17:43:01 +0700

    Added getters and setters for DataRecord parent

--- src/Model/DataRecord.java
+++ src/Model/DataRecord.java
@@ -6,4 +6,13 @@ public class DataRecord
 {
     private final ArrayList<DataRecord> children = new ArrayList<>();
     private DataRecord parent = null;
+
+    public DataRecord getParent()
+    {
+        return parent;
+    }
+    public void setParent(DataRecord newParent)
+    {
+        parent = newParent;
+    }
 }
