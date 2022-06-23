e8eb43305c486f1513f6030077fa26be6561dfb9
David <95734746+david-manalu@users.noreply.github.com>
2022-06-23 19:42:55 +0700

    Added Brand DataRecord and DataRecordPool (#2)
    * Added Brand DataRecord
    
    * Fixed Bracket Formatting on Brand DataRecord
    
    * Added Brand DataRecordPool
    
    * Fixed typing error on Brand DataRecordPool
    
    * Inlined member functions on Brand DataRecord
    
    Co-authored-by: adafcaefc <kjuanvincent@gmail.com>

---
+++ src/Model/DataPool/BrandPool.java
@@ -0,0 +1,10 @@
+package Model.DataPool;
+
+public class BrandPool extends DataRecordPool
+{
+    private static final BrandPool instance = new BrandPool();
+
+    private BrandPool() { super(null); }
+
+    public static BrandPool get() { return instance; }
+}
