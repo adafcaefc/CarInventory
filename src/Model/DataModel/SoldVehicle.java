f876ed2fdc48cc673648d8c30ed0cdcaba73923e
David <95734746+david-manalu@users.noreply.github.com>
2022-06-24 18:54:26 +0700

    Added SoldVehicle DataRecord

---
+++ src/Model/DataModel/SoldVehicle.java
@@ -0,0 +1,20 @@
+package Model.DataModel;
+
+import java.util.GregorianCalendar;
+
+public class SoldVehicle {
+    private String brandName;
+    private String modelName;
+    private Integer modelYear = 0;
+    private Boolean hasSunroof = false;
+    private Integer doorCount = 0;
+    private Integer seatCount = 0;
+    private Double fuelCapacity = 0.0;
+    private String VIN;
+    private String licensePlate;
+    private String color;
+    private Double mileage = 0.0;
+    private GregorianCalendar dateOfSale;
+    private Double paidAmount;
+
+}
