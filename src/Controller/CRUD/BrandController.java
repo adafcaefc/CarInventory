87766ec7904f608390144a319ae4394be09ce84e
adafcaefc <kjuanvincent@gmail.com>
2022-06-24 19:47:21 +0700

    Added BrandController class openDeleteWindow

--- src/Controller/CRUD/BrandController.java
+++ src/Controller/CRUD/BrandController.java
@@ -14,7 +14,25 @@ public class BrandController extends DataRecordController
     }
 
 
-
+    @Override
+    public void openDeleteWindow()
+    {
+        Brand brand = (Brand) getSelectedItem(BrandPool.get());
+        if (brand == null) { return; }
+        int brandIndex = BrandPool.get().getIndexForComponent(brand);
+        int childrenCount = brand.countChildren();
+        String deleteMsg = String.format("Are you sure you want to delete model no.%d (%s)?", brandIndex + 1, brand.getBrandName());
+        if (childrenCount > 0)
+        {
+            deleteMsg += "\nAll " + childrenCount + " models with this brand will also be deleted";
+        }
+        int choice = JOptionPane.showConfirmDialog(null, deleteMsg, "Delete Brands", JOptionPane.YES_NO_OPTION);
+        if (choice == JOptionPane.YES_OPTION)
+        {
+            BrandPool.get().unregisterComponent(brand);
+            updateListener.onDataModelsChanged();
+        }
+    }
 
     @Override
     public void loadViewTable()
