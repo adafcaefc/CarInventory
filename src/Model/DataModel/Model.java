6ae3392df937c4d8a6c72317b1a444232424a138
Thomas Teguh <thmstgh@gmail.com>
2022-06-24 18:35:14 +0700

    Added Model class fields

--- src/Model/DataModel/Model.java
+++ src/Model/DataModel/Model.java
@@ -1,3 +1,8 @@
 public class Model {
-    
+    private String modelName;
+    private Integer modelYear = 0;
+    private Boolean hasSunroof = false;
+    private Integer doorCount = 0;
+    private Integer seatCount = 0;
+    private Double fuelCapacity = 0.0;
 }
