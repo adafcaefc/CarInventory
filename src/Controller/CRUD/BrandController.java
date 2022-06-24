91074df2e48af5e45c19222497d24f58375740d4
adafcaefc <kjuanvincent@gmail.com>
2022-06-24 19:47:09 +0700

    Added BrandController class loadViewTable

--- src/Controller/CRUD/BrandController.java
+++ src/Controller/CRUD/BrandController.java
@@ -1,5 +1,32 @@
 package Controller.CRUD;
 
-public class BrandController
+import Model.DataModel.Brand;
+import Model.DataPool.BrandPool;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class BrandController extends DataRecordController
 {
+    public BrandController(JTable table, UpdateListener updateListener)
+    {
+        super(table, updateListener);
+    }
+
+
+
+
+    @Override
+    public void loadViewTable()
+    {
+        String[] header = new String[]{ "Brand Name" };
+        DefaultTableModel tableModel = new DefaultTableModel(header, 0);
+        for (var obj : BrandPool.get())
+        {
+            var brandObject = (Brand) obj;
+            tableModel.addRow(new Object[]{ brandObject.getBrandName() });
+        }
+        table.setModel(tableModel);
+        table.setDefaultEditor(Object.class, null); // disable editor
+    }
 }
