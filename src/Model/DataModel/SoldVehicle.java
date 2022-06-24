81c627077ad61fc81726556e142ed4ccef25ffa3
David <95734746+david-manalu@users.noreply.github.com>
2022-06-24 18:54:33 +0700

    Added getters and setters for SoldVehicle DataRecord

--- src/Model/DataModel/SoldVehicle.java
+++ src/Model/DataModel/SoldVehicle.java
@@ -17,4 +17,97 @@ public class SoldVehicle {
     private GregorianCalendar dateOfSale;
     private Double paidAmount;
 
+    public String getBrandName()
+    {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) { this.brandName = brandName; }
+
+    public String getModelName()
+    {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) { this.modelName = modelName; }
+
+    public Integer getModelYear()
+    {
+        return modelYear;
+    }
+
+    public void setModelYear(Integer modelYear) { this.modelYear = modelYear; }
+
+    public Boolean getHasSunroof()
+    {
+        return hasSunroof;
+    }
+
+    public void setHasSunroof(Boolean hasSunroof) { this.hasSunroof = hasSunroof; }
+
+    public Integer getDoorCount()
+    {
+        return doorCount;
+    }
+
+    public void setDoorCount(Integer doorCount) { this.doorCount = doorCount; }
+
+    public Integer getSeatCount()
+    {
+        return seatCount;
+    }
+
+    public void setSeatCount(Integer seatCount) { this.seatCount = seatCount; }
+
+    public Double getFuelCapacity()
+    {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(Double fuelCapacity) { this.fuelCapacity = fuelCapacity; }
+
+    public String getVIN()
+    {
+        return VIN;
+    }
+
+    public void setVIN(String VIN) { this.VIN = VIN; }
+
+    public String getLicensePlate()
+    {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) { this.licensePlate = licensePlate; }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public void setColor(String color) { this.color = color; }
+
+    public Double getMileage()
+    {
+        return mileage;
+    }
+
+    public void setMileage(Double mileage) { this.mileage = mileage; }
+
+    public GregorianCalendar getDateOfSale()
+    {
+        return dateOfSale;
+    }
+
+    public void setDateOfSale(GregorianCalendar dateOfSale) { this.dateOfSale = dateOfSale; }
+
+    public Double getPaidAmount()
+    {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(Double paidAmount)
+    {
+        this.paidAmount = paidAmount;
+    }
 }
