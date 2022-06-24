d7b7dab26e34620733134ccfc31ad2352633a9e1
Thomas Teguh <thmstgh@gmail.com>
2022-06-24 18:35:56 +0700

    Added Model getters and setters

--- src/Model/DataModel/Model.java
+++ src/Model/DataModel/Model.java
@@ -5,4 +5,65 @@ public class Model {
     private Integer doorCount = 0;
     private Integer seatCount = 0;
     private Double fuelCapacity = 0.0;
+
+
+    public String getModelName()
+    {
+        return this.modelName;
+    }
+
+    public void setModelName(String modelName)
+    {
+        this.modelName = modelName;
+    }
+
+    public Integer getModelYear()
+    {
+        return this.modelYear;
+    }
+
+    public void setModelYear(Integer modelYear)
+    {
+        this.modelYear = modelYear;
+    }
+
+    public Boolean getHasSunroof()
+    {
+        return this.hasSunroof;
+    }
+
+    public void setHasSunroof(Boolean newSunroof)
+    {
+        this.hasSunroof = newSunroof;
+    }
+
+    public Integer getDoorCount()
+    {
+        return this.doorCount;
+    }
+
+    public void setDoorCount(Integer newDoorCount)
+    {
+        this.doorCount = newDoorCount;
+    }
+
+    public Integer getSeatCount()
+    {
+        return this.seatCount;
+    }
+
+    public void setSeatCount(Integer newSeatCount)
+    {
+        this.seatCount = newSeatCount;
+    }
+
+    public Double getFuelCapacity()
+    {
+        return this.fuelCapacity;
+    }
+
+    public void setFuelCapacity(Double newFuelCapacity)
+    {
+        this.fuelCapacity = newFuelCapacity;
+    }
 }
