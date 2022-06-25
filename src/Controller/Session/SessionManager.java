c9a2f19af8627bc559c88ca15d14d6374d670a57
adafcaefc <kjuanvincent@gmail.com>
2022-06-25 18:57:24 +0700

    Cleanup SessionManager

--- src/Controller/Session/SessionManager.java
+++ src/Controller/Session/SessionManager.java
@@ -11,56 +11,34 @@ import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 
 // singleton
-public class SessionManager {
+public class SessionManager
+{
     User currentUser = null;
     String fileName = "JoeCarSession.dat";
-
-    private SessionManager() {
-    }
-
+    private SessionManager() { }
     private static final SessionManager instance = new SessionManager();
+    public static SessionManager get() { return instance; }
 
-    public static SessionManager get() {
-        return instance;
-    }
-
-    public boolean isLoggedIn() {
-        return currentUser != null;
-    }
-
-    public void logOut() {
-        currentUser = null;
-    }
-
-    public User getCurrentUser() {
+    public User getCurrentUser()
+    {
         return currentUser;
     }
 
-    public void loadSession() {
-        if (!new File(fileName).isFile()) {
-            return;
-        }
-        try {
-            FileInputStream file = new FileInputStream(fileName);
-            ObjectInputStream in = new ObjectInputStream(file);
-
-            currentUser = ((UserSession) in.readObject()).getUser();
-
-            in.close();
-            file.close();
-        } catch (IOException | ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(
-                    null, ex.getMessage(),
-                    "Error",
-                    JOptionPane.ERROR_MESSAGE);
-        }
+    public boolean isLoggedIn()
+    {
+        return currentUser != null;
     }
 
-    public void saveSession() {
-        if (!isLoggedIn()) {
-            return;
-        }
-        try {
+    public void logOut()
+    {
+        currentUser = null;
+    }
+
+    public void saveSession()
+    {
+        if (!isLoggedIn()) { return; }
+        try
+        {
             FileOutputStream file = new FileOutputStream(fileName);
             ObjectOutputStream out = new ObjectOutputStream(file);
 
@@ -68,7 +46,9 @@ public class SessionManager {
 
             out.close();
             file.close();
-        } catch (IOException ex) {
+        }
+        catch (IOException ex)
+        {
             JOptionPane.showMessageDialog(
                     null, ex.getMessage(),
                     "Error",
@@ -76,11 +56,35 @@ public class SessionManager {
         }
     }
 
-    public User logIn(String userName, String password) throws NoSuchAlgorithmException {
-        for (var obj : UserPool.get()) {
+    public void loadSession()
+    {
+        if (!new File(fileName).isFile()) { return; }
+        try
+        {
+            FileInputStream file = new FileInputStream(fileName);
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            currentUser = ((UserSession) in.readObject()).getUser();
+
+            in.close();
+            file.close();
+        }
+        catch (IOException | ClassNotFoundException  ex)
+        {
+            JOptionPane.showMessageDialog(
+                    null, ex.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public User logIn(String userName, String password) throws NoSuchAlgorithmException
+    {
+        for (var obj : UserPool.get())
+        {
             User user = (User) obj;
-            if (user.getUserName().equals(userName)
-                    && user.getPassword().equals(PasswordHelper.sha256Salted(password, user.getSalt()))) {
+            if (user.getUserName().equals(userName) && user.getPassword().equals(PasswordHelper.sha256Salted(password, user.getSalt())))
+            {
                 currentUser = user;
                 saveSession();
                 return user;
