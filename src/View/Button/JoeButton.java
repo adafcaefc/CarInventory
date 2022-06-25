ca67b1a9014c2802a45140aff5a1d6981ed5180e
Thomas Teguh <107668394+ThomasTgh@users.noreply.github.com>
2022-06-25 18:43:39 +0700

    Added UI utilities (#18)
    * Added JoeButton utility class
    
    * Added ShowReceipt utility class

---
+++ src/View/Button/JoeButton.java
@@ -0,0 +1,79 @@
+package View.Button;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class JoeButton extends JButton {
+    public static final Color SELECTED_COLOR = new Color(0x183265);
+    public static final Color UNSELECTED_COLOR = new Color(0x1F4690);
+
+    public static final Color HOVER_COLOR = new Color(0x3A5BA0);
+    public static final Color CLICKED_COLOR = new Color(0x5180E1);
+
+    public static final Color TEXT_COLOR = new Color(0xEFEFEF);
+    public static final Color DISABLED_TEXT_COLOR = new Color(0xF1EEE9);
+
+    public static final Font JOE_BUTTON_FONT = new Font("Century Gothic", Font.BOLD, 16);
+
+    public boolean isJoeSelected() {
+        return isJoeSelected;
+    }
+
+    public void setJoeSelected(boolean joeSelected) {
+        setSelected(joeSelected);
+        isJoeSelected = joeSelected;
+        updateAppearance();
+    }
+
+    public boolean isJoeEnabled() {
+        return isJoeEnabled;
+    }
+
+    public void setJoeEnabled(boolean joeEnabled) {
+        setEnabled(joeEnabled);
+        isJoeEnabled = joeEnabled;
+        updateAppearance();
+    }
+
+    boolean isJoeSelected = false;
+    boolean isJoeEnabled = true;
+
+    private Color getSelectedColor() {
+        return isJoeSelected ? SELECTED_COLOR : UNSELECTED_COLOR;
+    }
+
+    private Color getTextColor() {
+        return isJoeEnabled ? TEXT_COLOR : DISABLED_TEXT_COLOR;
+    }
+
+    public void updateAppearance() {
+        setBackground(getSelectedColor());
+        setForeground(getTextColor());
+    }
+
+    public JoeButton(String text) {
+        super(text);
+        // setBorder(BorderFactory.createEmptyBorder());
+        setFocusPainted(false);
+        setBorderPainted(false);
+        setRolloverEnabled(false);
+        // setContentAreaFilled(false);
+        updateAppearance();
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent evt) {
+                if (isEnabled()) {
+                    setBackground(HOVER_COLOR);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent evt) {
+                setBackground(getSelectedColor());
+            }
+        });
+        setFont(JOE_BUTTON_FONT);
+    }
+}
---
+++ src/View/Form/ShowReceipt.java
@@ -0,0 +1,52 @@
+package View.Form;
+
+import Model.DataModel.SoldVehicle;
+import View.Utility.SpringUtilities;
+
+import javax.swing.*;
+
+public class ShowReceipt extends BaseForm {
+    private final SoldVehicle soldVehicle;
+
+    @Override
+    public void bindButtons(JButton okButton, JButton cancelButton) {
+        okButton.addActionListener(e -> dispose());
+        cancelButton.addActionListener(e -> dispose());
+    }
+
+    public ShowReceipt(SoldVehicle soldVehicle) {
+        super();
+
+        this.soldVehicle = soldVehicle;
+
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setContentPane(mainBody);
+        bindButtons(okButton, cancelButton);
+
+        mainBody.setLayout(new SpringLayout());
+
+        addLabeledComponent("Vehicle ID", new JLabel(soldVehicle.getVIN()));
+        addLabeledComponent("Plate Number", new JLabel(soldVehicle.getLicensePlate()));
+        addLabeledComponent("Colour", new JLabel(soldVehicle.getColor()));
+        addLabeledComponent("Mileage", new JLabel(String.valueOf(soldVehicle.getMileage())));
+        addLabeledComponent("Model", new JLabel(soldVehicle.getModelName()));
+        addLabeledComponent("Model Year", new JLabel(String.valueOf(soldVehicle.getModelYear())));
+        addLabeledComponent("Sunroof", new JLabel(soldVehicle.getHasSunroof() ? "Yes" : "No"));
+        addLabeledComponent("Number of Doors", new JLabel(String.valueOf(soldVehicle.getDoorCount())));
+        addLabeledComponent("Number of Seats", new JLabel(String.valueOf(soldVehicle.getSeatCount())));
+        addLabeledComponent("Fuel Capacity", new JLabel(String.valueOf(soldVehicle.getFuelCapacity())));
+        addLabeledComponent("Brand", new JLabel(soldVehicle.getBrandName()));
+
+        mainBody.add(cancelButton);
+        mainBody.add(okButton);
+
+        setTitle("Vehicle Sold");
+
+        cancelButton.setVisible(false);
+        okButton.setText("Close");
+
+        SpringUtilities.makeCompactGrid(mainBody, 12, 2, 6, 6, 6, 6);
+
+        pack();
+    }
+}
