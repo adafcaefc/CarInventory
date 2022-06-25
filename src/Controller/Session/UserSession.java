28ce73b6c63a71f8828b78f16b85e7e854bd6e3f
Thomas Teguh <thmstgh@gmail.com>
2022-06-25 18:34:32 +0700

    Added UserSession class

---
+++ src/Controller/Session/UserSession.java
@@ -0,0 +1,32 @@
+package Controller.Session;
+
+import Model.DataModel.User;
+import Model.DataPool.UserPool;
+
+public class UserSession implements java.io.Serializable {
+    private final String userName;
+    private final String password;
+    private final String salt;
+
+    public boolean isTheSame(User user) {
+        return user.getUserName().equals(userName)
+                && user.getPassword().equals(password)
+                && user.getSalt().equals(salt);
+    }
+
+    public UserSession(User user) {
+        this.userName = user.getUserName();
+        this.password = user.getPassword();
+        this.salt = user.getSalt();
+    }
+
+    public User getUser() {
+        for (var obj : UserPool.get()) {
+            User user = (User) obj;
+            if (isTheSame(user)) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
