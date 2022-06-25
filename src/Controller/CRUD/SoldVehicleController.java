40aa0dfd388a49b79576c7980a8e32a3205f04ae
adafcaefc <kjuanvincent@gmail.com>
2022-06-25 18:54:05 +0700

    Cleanup CRUD controllers

--- src/Controller/CRUD/SoldVehicleController.java
+++ src/Controller/CRUD/SoldVehicleController.java
@@ -1,29 +1,25 @@
+package Controller.CRUD;
+
+import Model.DataModel.SoldVehicle;
+import Model.DataPool.SoldVehiclePool;
+import View.Form.InputForm.SoldVehicleForm;
+import View.Form.ShowReceipt;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
-import Model.DataPool.SoldVehiclePool;
-import Model.DataModel.SoldVehicle;
-
-import java.util.ArrayList;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
-public class SoldVehicleController extends DataRecordController {
-
-    @Override
-    public void openModifyWindow(JFrame parent) {
-
-    }
-
-    public void openShowWindow(JFrame parent) {
-
-    }
-
-    public SoldVehicleController(JTable table, UpdateListener updateListener) {
+public class SoldVehicleController extends DataRecordController
+{
+    public SoldVehicleController(JTable table, UpdateListener updateListener)
+    {
         super(table, updateListener);
     }
 
     @Override
-    public void openCreateWindow(JFrame parent) {
+    public void openCreateWindow(JFrame parent)
+    {
         JOptionPane.showMessageDialog(
                 null,
                 "You cannot insert sales manually",
@@ -32,32 +28,52 @@ public class SoldVehicleController extends DataRecordController {
     }
 
     @Override
-    public void openDeleteWindow() {
+    public void openModifyWindow(JFrame parent)
+    {
         SoldVehicle soldVehicle = (SoldVehicle) getSelectedItem(SoldVehiclePool.get());
-        if (soldVehicle == null) {
-            return;
-        }
+        if (soldVehicle == null) { return; }
+        SoldVehicleForm form = new SoldVehicleForm(parent, true, soldVehicle);
+        form.bindUpdateListener(updateListener);
+        form.setVisible(true);
+    }
+
+    public void openShowWindow(JFrame parent)
+    {
+        SoldVehicle soldVehicle = (SoldVehicle) getSelectedItem(SoldVehiclePool.get());
+        if (soldVehicle == null) { return; }
+        ShowReceipt form = new ShowReceipt(soldVehicle);
+        form.setVisible(true);
+    }
+
+    @Override
+    public void openDeleteWindow()
+    {
+        SoldVehicle soldVehicle = (SoldVehicle) getSelectedItem(SoldVehiclePool.get());
+        if (soldVehicle == null) { return; }
         int vehicleIndex = SoldVehiclePool.get().getIndexForComponent(soldVehicle);
-        String deleteMsg = String.format("Are you sure you want to delete sales no.%d from the Sales Log?",
-                vehicleIndex + 1);
+        String deleteMsg = String.format("Are you sure you want to delete sales no.%d from the Sales Log?", vehicleIndex + 1);
         int choice = JOptionPane.showConfirmDialog(null, deleteMsg, "Delete Sales Log", JOptionPane.YES_NO_OPTION);
-        if (choice == JOptionPane.YES_OPTION) {
+        if (choice == JOptionPane.YES_OPTION)
+        {
             SoldVehiclePool.get().unregisterComponent(soldVehicle);
             updateListener.onDataModelsChanged();
         }
     }
 
     @Override
-    public void loadViewTable() {
-        String[] header = new String[] {
-                "Vehicle ID",
-                "Plate Number",
-                "Paid Amount",
-                "Sale Date"
-        };
+    public void loadViewTable()
+    {
+        String[] header = new String[]
+                {
+                        "Vehicle ID",
+                        "Plate Number",
+                        "Paid Amount",
+                        "Sale Date"
+                };
 
         var tableDataMatrix = new ArrayList<ArrayList<Object>>();
-        for (var obj : SoldVehiclePool.get()) {
+        for (var obj : SoldVehiclePool.get())
+        {
             SoldVehicle vehicleObject = (SoldVehicle) obj;
             ArrayList<Object> innerData = new ArrayList<>();
             innerData.add(vehicleObject.getVIN());
@@ -68,9 +84,7 @@ public class SoldVehicleController extends DataRecordController {
         }
 
         DefaultTableModel tableModel = new DefaultTableModel(header, 0);
-        for (ArrayList<Object> row : tableDataMatrix) {
-            tableModel.addRow(row.toArray());
-        }
+        for (ArrayList<Object> row : tableDataMatrix) { tableModel.addRow(row.toArray()); }
 
         table.setModel(tableModel);
         table.setDefaultEditor(Object.class, null); // disable editor
--- src/Controller/CRUD/VehicleController.java
+++ src/Controller/CRUD/VehicleController.java
@@ -1,25 +1,71 @@
 package Controller.CRUD;
 
-import java.util.ArrayList;
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-
 import Model.DataModel.Vehicle;
 import Model.DataPool.VehiclePool;
+import View.Form.InputForm.VehicleForm;
 
-public class VehicleController extends DataRecordController {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+
+public class VehicleController extends DataRecordController
+{
+    public VehicleController(JTable table, UpdateListener updateListener)
+    {
+        super(table, updateListener);
+    }
+
     @Override
-    public void loadViewTable() {
-        String[] header = new String[] {
-                "Vehicle ID",
-                "Plate Number",
-                "Colour",
-                "Mileage",
-                "Model"
-        };
+    public void openCreateWindow(JFrame parent)
+    {
+        VehicleForm form = new VehicleForm(parent, false, null);
+        form.bindUpdateListener(updateListener);
+        form.setVisible(true);
+    }
+
+    @Override
+    public void openModifyWindow(JFrame parent)
+    {
+        Vehicle vehicle = (Vehicle) getSelectedItem(VehiclePool.get());
+        if (vehicle == null) { return; }
+        VehicleForm form = new VehicleForm(parent, true, vehicle);
+        form.bindUpdateListener(updateListener);
+        form.setVisible(true);
+    }
+
+    @Override
+    public void openDeleteWindow()
+    {
+        Vehicle vehicle = (Vehicle) getSelectedItem(VehiclePool.get());
+        if (vehicle == null) { return; }
+        int vehicleIndex = VehiclePool.get().getIndexForComponent(vehicle);
+        String deleteMsg = String.format(
+                "Are you sure you want to delete vehicle no.%d with Vehicle Identification Number %s?",
+                vehicleIndex + 1,
+                vehicle.getVIN());
+        int choice = JOptionPane.showConfirmDialog(null, deleteMsg, "Delete Vehicle", JOptionPane.YES_NO_OPTION);
+        if (choice == JOptionPane.YES_OPTION)
+        {
+            VehiclePool.get().unregisterComponent(vehicle);
+            updateListener.onDataModelsChanged();
+        }
+    }
+
+    @Override
+    public void loadViewTable()
+    {
+        String[] header = new String[]
+                {
+                        "Vehicle ID",
+                        "Plate Number",
+                        "Colour",
+                        "Mileage",
+                        "Model"
+                };
 
         var tableDataMatrix = new ArrayList<ArrayList<Object>>();
-        for (var obj : VehiclePool.get()) {
+        for (var obj : VehiclePool.get())
+        {
             Vehicle vehicleObject = (Vehicle) obj;
             ArrayList<Object> innerData = new ArrayList<>();
             innerData.add(vehicleObject.getVIN());
@@ -31,44 +77,10 @@ public class VehicleController extends DataRecordController {
         }
 
         DefaultTableModel tableModel = new DefaultTableModel(header, 0);
-        for (ArrayList<Object> row : tableDataMatrix) {
-            tableModel.addRow(row.toArray());
-        }
+        for (ArrayList<Object> row : tableDataMatrix) { tableModel.addRow(row.toArray()); }
 
         table.setModel(tableModel);
         table.setDefaultEditor(Object.class, null); // disable Editing
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-    }
-
-    @Override
-    public void openDeleteWindow() {
-        Vehicle vehicle = (Vehicle) getSelectedItem(VehiclePool.get());
-        if (vehicle == null) {
-            return;
-        }
-        int vehicleIndex = VehiclePool.get().getIndexForComponent(vehicle);
-        String deleteMsg = String.format(
-                "Are you sure you want to delete vehicle no.%d with Vehicle Identification Number %s?",
-                vehicleIndex + 1,
-                vehicle.getVIN());
-        int choice = JOptionPane.showConfirmDialog(null, deleteMsg, "Delete Vehicle", JOptionPane.YES_NO_OPTION);
-        if (choice == JOptionPane.YES_OPTION) {
-            VehiclePool.get().unregisterComponent(vehicle);
-            updateListener.onDataModelsChanged();
-        }
-    }
-
-    @Override
-    public void openModifyWindow(JFrame parent) {
-
-    }
-
-    @Override
-    public void openCreateWindow(JFrame parent) {
-
-    }
-
-    public VehicleController(JTable table, UpdateListener updateListener) {
-        super(table, updateListener);
     }
 }
