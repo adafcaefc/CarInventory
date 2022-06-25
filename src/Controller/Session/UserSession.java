d844d3bba6f39af7a7a93ad978e17372e2d0c6cf
adafcaefc <kjuanvincent@gmail.com>
2022-06-25 18:57:32 +0700

    Cleanup UserSession

--- src/Controller/Session/UserSession.java
+++ src/Controller/Session/UserSession.java
@@ -3,27 +3,33 @@ package Controller.Session;
 import Model.DataModel.User;
 import Model.DataPool.UserPool;
 
-public class UserSession implements java.io.Serializable {
+public class UserSession implements java.io.Serializable
+{
     private final String userName;
     private final String password;
     private final String salt;
 
-    public boolean isTheSame(User user) {
-        return user.getUserName().equals(userName)
-                && user.getPassword().equals(password)
-                && user.getSalt().equals(salt);
-    }
-
-    public UserSession(User user) {
+    public UserSession(User user)
+    {
         this.userName = user.getUserName();
         this.password = user.getPassword();
         this.salt = user.getSalt();
     }
 
-    public User getUser() {
-        for (var obj : UserPool.get()) {
+    public boolean isTheSame(User user)
+    {
+        return user.getUserName().equals(userName)
+                && user.getPassword().equals(password)
+                && user.getSalt().equals(salt);
+    }
+
+    public User getUser()
+    {
+        for (var obj : UserPool.get())
+        {
             User user = (User) obj;
-            if (isTheSame(user)) {
+            if (isTheSame(user))
+            {
                 return user;
             }
         }
